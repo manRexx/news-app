5749116c53dadd23c98b23b5f1d37bf9bf65618d
Manik-Rawat <manikrawat@outlook.com>
2020-12-27 00:29:45 +0530

    project initialize

---
+++ android/app/src/main/kotlin/com/manik/feeder/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.manik.feeder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Text('Flash'),
+            Text('News',style: TextStyle(
+              color: Colors.blue
+            ),)
+          ],
+        ),
+      ),
+    );
+  }
+}
