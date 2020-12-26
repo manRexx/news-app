1625579edd5bc41bef3ccb4ec012bd960db7788d
Manik-Rawat <manikrawat@outlook.com>
2020-12-27 01:23:00 +0530

    categories added

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,3 +1,5 @@
+import 'package:feeder/helper/data.dart';
+import 'package:feeder/models/category_model.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -6,11 +8,22 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  List<CategoryModel> categories=new List<CategoryModel>();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    categories=getCategories();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text('Flash'),
             Text('News',style: TextStyle(
@@ -18,6 +31,63 @@ class _HomeState extends State<Home> {
             ),)
           ],
         ),
+        elevation: 0.0,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 60.0),
+              height: 70.0,
+              child: ListView.builder(
+                  itemCount:categories.length,
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index){
+                    return CategoryTile(
+                      imageUrl: categories[index].imageUrl,
+                      categoryName: categories[index].categoryName,
+                    );
+                  }),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class CategoryTile extends StatelessWidget {
+
+  final imageUrl, categoryName;
+  CategoryTile({this.imageUrl,this.categoryName});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(right: 16.0),
+      child: Stack(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(6.0),
+              child: Image.network(imageUrl, width: 120.0, height: 60.0,fit: BoxFit.cover,)
+          ),
+          Container(
+            alignment: Alignment.center,
+            width: 120.0, height: 60.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(6.0),
+              color: Colors.black26,
+            ),
+            child: Text(
+              categoryName,style: TextStyle(
+              color: Colors.white,
+              fontSize: 14,
+              fontWeight: FontWeight.w500
+            ),
+            ),
+          )
+        ],
       ),
     );
   }
