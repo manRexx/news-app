be69546a654080f72eeea6a446b4db141750ac66
Manik-Rawat <manikrawat@outlook.com>
2020-12-28 00:46:37 +0530

    page made for articles

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -3,6 +3,7 @@ import 'package:feeder/helper/data.dart';
 import 'package:feeder/helper/news.dart';
 import 'package:feeder/models/article_model.dart';
 import 'package:feeder/models/category_model.dart';
+import 'package:feeder/views/article_view.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -84,6 +85,7 @@ class _HomeState extends State<Home> {
                         imageUrl: articles[index].urlToImage,
                         title: articles[index].title,
                         desc: articles[index].description,
+                        url: articles[index].url,
                       );
                     }),
               )
@@ -139,33 +141,43 @@ class CategoryTile extends StatelessWidget {
 
 class BlogTile extends StatelessWidget {
 
-  final String imageUrl, title, desc;
-  BlogTile({@required this.imageUrl,@required this.title,@required this.desc});
+  final String imageUrl, title, desc, url;
+  BlogTile({@required this.imageUrl,@required this.title,@required this.desc,@required this.url});
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(bottom: 16.0),
-      child: Column(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(6.0),
-              child: Image.network(imageUrl)
-          ),
-          SizedBox(height: 8.0,),
-          Text(title, style: TextStyle(
-            fontSize: 18.0,
-            color: Colors.black87,
-            fontWeight: FontWeight.w500,
-          ),
-          ),
-          SizedBox(height: 8.0,),
-          Text(
-            desc,
-            style: TextStyle(
-              color: Colors.black54,
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(
+          builder: (context)=>ArticleView(
+            blogUrl: url,
+          )
+        )
+        );
+      },
+      child: Container(
+        margin: EdgeInsets.only(bottom: 16.0),
+        child: Column(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(6.0),
+                child: Image.network(imageUrl)
             ),
-          ),
-        ],
+            SizedBox(height: 8.0,),
+            Text(title, style: TextStyle(
+              fontSize: 18.0,
+              color: Colors.black87,
+              fontWeight: FontWeight.w500,
+            ),
+            ),
+            SizedBox(height: 8.0,),
+            Text(
+              desc,
+              style: TextStyle(
+                color: Colors.black54,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
